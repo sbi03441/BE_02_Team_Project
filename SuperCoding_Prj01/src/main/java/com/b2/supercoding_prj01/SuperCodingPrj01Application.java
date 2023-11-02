00cc582f65ed740c3dc30904af60ef7d85661335
Kim Jae Hyuk <rlawogur816@gmail.com>
2023-11-02 15:33:50 +0900

    Package 및 Spring build

---
+++ SuperCoding_Prj01/src/main/java/com/b2/supercoding_prj01/SuperCodingPrj01Application.java
@@ -0,0 +1,13 @@
+package com.b2.supercoding_prj01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperCodingPrj01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperCodingPrj01Application.class, args);
+    }
+
+}
---
+++ SuperCoding_Prj01/src/test/java/com/b2/supercoding_prj01/SuperCodingPrj01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b2.supercoding_prj01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperCodingPrj01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
